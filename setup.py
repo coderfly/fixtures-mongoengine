fa4d74fde5162c75271f721ce024e0b487c30d0e
Vitaly Nikitin <coderfly@CODERFLY-NB-W7.ld.yandex.ru>
2016-08-23 16:34:25 +0300

    Added download_url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='fixtures_mongoengine',
       description='MongoEngine Fixtures.',
       author='Vitaly Nikitin',
       url='https://github.com/coderfly/fixtures-mongoengine/',
+      download_url='https://github.com/coderfly/fixtures-mongoengine/tarball/1.0.0',
       install_requires=['mongoengine>=0.8.6'],
       packages=['fixtures_mongoengine'],
       keywords=['testing', 'fixtures', 'mongoengine']
