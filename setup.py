816fcbe091ae59cb55441b11c05e8b7147dfd8cc
Vitaly Nikitin <coderfly@CODERFLY-NB-W7.ld.yandex.ru>
2016-08-24 13:05:49 +0300

    Changed setup utils

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='fixtures_mongoengine',
       version='1.0.0',
       description='MongoEngine Fixtures.',
       author='Vitaly Nikitin',
+      author_email='alaris.nik@gmail.com',
       url='https://github.com/coderfly/fixtures-mongoengine/',
       download_url='https://github.com/coderfly/fixtures-mongoengine/tarball/1.0.0',
       install_requires=['mongoengine>=0.8.6'],
